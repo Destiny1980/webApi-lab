40465ef6d86e4b7b9a93c228540a63dc0a88495e
Destiny1980 <truelovenotreason@gmail.com>
2025-02-07 21:55:56 +0800

    lab 4
    lab 4

---
+++ films.ts
@@ -0,0 +1,15 @@
+const films = [
+{
+     id:1,
+     title: "First love",
+     duration: 120
+},
+{
+    id:2,
+     title: "My love",
+     duration: 240
+},
+
+]
+
+export default films;
---
+++ out/films.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const films = [
+    {
+        id: 1,
+        title: "First love",
+        duration: 120
+    },
+    {
+        id: 2,
+        title: "My love",
+        duration: 240
+    },
+];
+exports.default = films;
